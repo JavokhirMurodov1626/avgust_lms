a8606d71c898e44ab773abfd84cc5d8308be08bf
Murodov Javohir <javohir.murodov.9238@gmail.com>
2025-05-09 00:41:35 +0500

    -add login animation

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,23 +1,50 @@
 import { LoginForm } from "@/features/Login/LoginForm";
-import { LoginSwiper } from "@/features/LoginSwiper";
+// import { LoginSwiper } from "@/features/LoginSwiper";
+import { LoginAnimation } from "@/features/Login/Animation";
 
 const LoginPage = () => {
   return (
     <section className="relative h-screen bg-[url('/images/bg_gradient.svg')] bg-cover">
-      <div className="grid grid-cols-2 gap-6  max-w-[1440px] w-full mx-auto  h-full">
-        <div className="left bg-white p-10 rounded-lg self-center">
-          <LoginSwiper />
+      <div className="flex w-full mx-auto  h-full ">
+        <div className="left w-3/5 bg-white pb-6 px-5">
+          {/* <LoginSwiper /> */}
+          {/* <video
+            autoPlay
+            loop
+            muted
+            playsInline
+            style={{
+              backgroundImage:
+                'url("https://uploads-ssl.webflow.com/5fcf5cffc0333cf6b6778946/5fe48b14d7bed20939d36012_loop-faster-poster-00001.jpg")',
+              objectFit: "cover",
+            }}
+            data-wf-ignore="true"
+            className="w-full h-full"
+          >
+            <source
+              src="https://uploads-ssl.webflow.com/5fcf5cffc0333cf6b6778946/5fe48b14d7bed20939d36012_loop-faster-transcode.mp4"
+              data-wf-ignore="true"
+            />
+            <source
+              src="https://uploads-ssl.webflow.com/5fcf5cffc0333cf6b6778946/5fe48b14d7bed20939d36012_loop-faster-transcode.webm"
+              data-wf-ignore="true"
+            />
+          </video> */}
+
+          <LoginAnimation />
         </div>
 
-        <div className="right  max-w-[500px] bg-white p-10 rounded-lg self-center">
-          <h1 className="text-3xl text-center font-interSemiBold mb-3 text-slate-800">
-            Tizimga kirish
-          </h1>
-          <p className="text-center text-slate-500 text-sm mb-5">
-            Avgust LMS ga xush kelibsiz
-          </p>
-          <div className="">
-            <LoginForm />
+        <div className="right w-2/5 self-center px-6">
+          <div className="mx-auto max-w-[600px] w-full bg-white p-10 rounded-lg">
+            <h1 className="text-3xl text-center font-interSemiBold mb-3 text-slate-800">
+              Tizimga kirish
+            </h1>
+            <p className="text-center text-slate-500 text-sm mb-5">
+              Avgust LMS ga xush kelibsiz
+            </p>
+            <div className="">
+              <LoginForm />
+            </div>
           </div>
         </div>
       </div>
---
+++ src/features/Login/Animation.tsx
@@ -0,0 +1,7 @@
+'use client'
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+
+
+export const LoginAnimation =()=>{
+    return <DotLottieReact src="/videos/D3wMOupP9x.lottie" loop autoplay />
+}
